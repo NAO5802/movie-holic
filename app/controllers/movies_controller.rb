fb6d16dd65b8d8867d47dc7601c16c1b9a09b0a2
NAO5802 <i100185b@gmail.com>
2019-12-17 14:09:03 +0900

    nameのストロングパラメーターを許可

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,20 @@
 class MoviesController < ApplicationController
+  before_action :redirect_to_signin
+  # , except: :index
+
   def index
 
   end
 
   def show
     
+  end
+
+
+  private
+
+  def redirect_to_signin
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
 
