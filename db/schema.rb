3a4e64a5fd52ee62a30e36568b17e7a83d1a0960
NAO5802 <i100185b@gmail.com>
2019-12-23 17:15:39 +0900

    fix users table

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,8 @@ ActiveRecord::Schema.define(version: 2019_12_18_060400) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
