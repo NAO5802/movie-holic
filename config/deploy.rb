8ef324d9de79542b7f215fe59cf45b80034d1cde
NAO5802 <i100185b@gmail.com>
2019-12-26 12:07:29 +0900

    unicorn setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,8 @@ set :linked_files, %w{ config/credentials.yml.enc }
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
   end
 
   desc 'upload credentials.yml.enc'
